88167d17333a151edf7ff0de67ecad125091f1b3
KayotPoziloy <yakhont04@mail.ru>
2024-04-22 21:08:10 +0300

    исправлен метод градиентного спуска - лабораторная работа 4

--- MO/lab4.py
+++ MO/lab4.py
@@ -2,43 +2,46 @@ import math
 
 
 # Шаг 1 основная функция
-def f(x1, x2):
-    return math.exp(-x1 ** 2 + x2 ** 2 + 2 * x1 * x2 - x2)
+def f(x, y):
+    return x**4 + y**4 - 4*x*y
 
 
-eps = 0.2
-h = 0.3
+eps = 0.000001
+h = 0.1
 
 
 # Шаг 2 производная по x1 и по x2
-def fx1(x1, x2):
-    return -1 * (2 * math.exp(x2 ** 2) * x1 - 2 * x2 * math.exp(x2 ** 2)) * math.exp(-x1 ** 2 + 2 * x2 * x1 - x2)
+def fx(x, y):
+    return 4 * (x**3 - y)
 
 
-def fx2(x1, x2):
-    return (2 * x2 + 2 * x1 - 1) * math.exp(x2 ** 2 + 2 * x1 * x2 - x2 - x1 ** 2)
+def fy(x, y):
+    return 4 * y**3 - 4 * x
 
 
 # Шаг 3
-x01 = 0
-x02 = 0
-X0 = [x01, x02]
-grad_f = [fx1(x01, x02), fx2(x01, x02)]
+x0 = 0.1
+y0 = 0.1
 
+x_old = x0
+y_old = y0
 while True:
     # Шаг 4
-    x1_old = x01
-    x2_old = x02
-    x1_new = x1_old - h * grad_f[0]
-    x2_new = x2_old - h * grad_f[1]
-    X = [x1_new, x2_new]
+    dx = fx(x_old, y_old)
+    dy = fy(x_old, y_old)
 
+    x_new = x_old - h * dx
+    y_new = y_old - h * dy
+
+    M = [x_new, y_new]
+    print("M: ", M)
     # Шаг 5
-    f_old = f(x1_old, x2_old)
-    f_new = f(x1_new, x2_new)
-
-    print(f_old, f_new)
+    f_old = f(x_old, y_old)
+    f_new = f(x_new, y_new)
     # Шаг 6
     if abs(f_new - f_old) < eps:
-        print(X)
+        print(M)
         break
+
+    x_old = x_new
+    y_old = y_new
