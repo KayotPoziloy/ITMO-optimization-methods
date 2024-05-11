8b063c7ca9b525ae4fb09687549276581e908b02
KayotPoziloy <yakhont04@mail.ru>
2024-05-11 10:53:04 +0300

    готовый генетический алгоритм

--- MO/lab 6.py
+++ MO/lab 6.py
@@ -9,6 +9,21 @@ distances = [
 ]
 
 
+# функция считает расстояние между двумя городами
+def distance(point1, point2):
+    return distances[point1][point2]
+
+
+# функция считает полное расстояние маршрута
+def all_distance(being):
+    summ_of_distance = 0
+    for i in range(4):
+        # print(being[i])
+        summ_of_distance += distance(being[i], being[i+1])
+    summ_of_distance += distance(being[-1], being[0])
+    return summ_of_distance
+
+
 # функция возвращает начальную популяцию
 def start_population():
     population = []
@@ -28,35 +43,33 @@ def parents(population):
     return random_list
 
 
-# функция скрещивает две особи. Точка разрыва после второго генома
-# сделать точку разрыва в рандомном месте
+# функция скрещивает две особи
 def cross(parents):
     break_point = random.randint(1, 4)
+    print("bp:", break_point)
 
     descendants = []
     first_child = []
     second_child = []
 
-    first_child.append(parents[0][0])
-    first_child.append(parents[0][1])
-    second_child.append(parents[1][0])
-    second_child.append(parents[1][1])
+    for i in range(0, break_point):
+        first_child.append(parents[0][i])
+        second_child.append(parents[1][i])
 
-    for i in range(2, 5):
+    for i in range(break_point, 5):
         first_genome = parents[0][i]
         second_genome = parents[1][i]
         if second_genome not in first_child:
             first_child.append(second_genome)
         if first_genome not in second_child:
             second_child.append(first_genome)
-    for i in range(2, 5):
+    for i in range(break_point, 5):
         first_genome = parents[0][i]
         second_genome = parents[1][i]
         if first_genome not in first_child:
             first_child.append(first_genome)
         if second_genome not in second_child:
             second_child.append(second_genome)
-    # заполнять детей недостающими числами
 
     descendants.append(first_child)
     descendants.append(second_child)
@@ -64,7 +77,22 @@ def cross(parents):
 
 
 population = start_population()
-par = parents(population)
-print(population)
-print(par)
-print(cross(par))
+print("начальная популяция", population)
+for _ in range(5):
+    par = parents(population)
+    print("родители", par)
+    crossover = cross(par)
+    print("потомство", crossover)
+    population.append(crossover[0])
+    population.append(crossover[1])
+
+    # сортируем популяции по сумме расстояний в порядке возрастания
+    population.sort(key=lambda being: all_distance(being))
+    print("популяция с потомством", population)
+
+    # итоговое поколение
+    population = population[:-2]
+
+    for i in range(len(population)):
+        print("суммa расстояний", population[i], all_distance(population[i]))
+
