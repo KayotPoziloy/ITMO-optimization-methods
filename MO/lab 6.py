49b5aa49383953c5921d0ee713c50069c435b119
KayotPoziloy <yakhont04@mail.ru>
2024-05-13 23:04:15 +0300

    генетический алгоритм по Селиной

--- MO/lab 6.py
+++ MO/lab 6.py
@@ -24,7 +24,7 @@ def all_distance(being):
     return summ_of_distance
 
 
-# функция возвращает начальную популяцию
+# функция определяет начальную популяцию
 def start_population():
     population = []
     # available_distances = distances
@@ -37,62 +37,95 @@ def start_population():
     return population
 
 
-# функция вычленяет две особи из популяции
+# функция определяет пары родителей
 def parents(population):
-    random_list = random.sample(population, 2)
+    random_list = random.sample(population, 4)
     return random_list
 
 
+def cross(parent, child, bp):
+    start_index = bp + 1   # начальный индекс, с которого начнется проход по списку
+
+    for i in range(start_index, start_index + len(parent)):
+        # index = 0
+        index = i % len(parent)  # геном начинающийся со второго выделенного элемента
+        gene = parent[index]
+        # print(i % len(parent))
+        if gene not in child:
+            for j in range(0, 5):
+                if child[j] == "-":
+                    child[j] = gene
+                    break
+    return child
+
 # функция скрещивает две особи
-def cross(parents):
-    break_point = random.randint(1, 4)
-    print("bp:", break_point)
+def crossover(parents):
+    first_break_point = random.randint(0, 3)
+    second_break_point = random.randint(first_break_point + 2, 5)
+    print("1 bp:", first_break_point)
+    print("2 bp:", second_break_point)
 
     descendants = []
-    first_child = []
-    second_child = []
+    first_child = ["-"] * 5
+    second_child = ["-"] * 5
+    third_child = ["-"] * 5
+    fourth_child = ["-"] * 5
 
-    for i in range(0, break_point):
-        first_child.append(parents[0][i])
-        second_child.append(parents[1][i])
+    # сохраняются выделенные зоны
+    for i in range(first_break_point, second_break_point):
+        first_child[i] = parents[0][1][i]
+        second_child[i] = parents[0][0][i]
+        third_child[i] = parents[1][1][i]
+        fourth_child[i] = parents[1][0][i]
 
-    for i in range(break_point, 5):
-        first_genome = parents[0][i]
-        second_genome = parents[1][i]
-        if second_genome not in first_child:
-            first_child.append(second_genome)
-        if first_genome not in second_child:
-            second_child.append(first_genome)
-    for i in range(break_point, 5):
-        first_genome = parents[0][i]
-        second_genome = parents[1][i]
-        if first_genome not in first_child:
-            first_child.append(first_genome)
-        if second_genome not in second_child:
-            second_child.append(second_genome)
+    print(first_child)
+    print(second_child)
+    print(third_child)
+    print(fourth_child)
+
+    first_child = cross(parents[0][0], first_child, first_break_point)
+    second_child = cross(parents[0][1], second_child, first_break_point)
+    third_child = cross(parents[1][0], third_child, first_break_point)
+    fourth_child = cross(parents[1][1], fourth_child, first_break_point)
 
     descendants.append(first_child)
     descendants.append(second_child)
+    descendants.append(third_child)
+    descendants.append(fourth_child)
     return descendants
 
 
 population = start_population()
 print("начальная популяция", population)
+
 for _ in range(5):
     par = parents(population)
-    print("родители", par)
-    crossover = cross(par)
-    print("потомство", crossover)
-    population.append(crossover[0])
-    population.append(crossover[1])
+    chunk_size = 2
+    chunks = [par[i:i + chunk_size] for i in range(0, len(par), chunk_size)]
+    print("пары родителей", chunks)
+    crosses = crossover(chunks)
+    print("потомство", crosses)
+    population.append(crosses[0])
+    population.append(crosses[1])
+    population.append(crosses[2])
+    population.append(crosses[3])
 
-    # сортируем популяции по сумме расстояний в порядке возрастания
-    population.sort(key=lambda being: all_distance(being))
-    print("популяция с потомством", population)
+    # сортируем популяцию
+    uniq_population = []
+    for i in population:
+        if i not in uniq_population:
+            uniq_population.append(i)
+    uniq_population.sort(key=lambda being: all_distance(being))
 
     # итоговое поколение
-    population = population[:-2]
+    sorted_population = []
+    cnt = 0
+    while cnt < 4:
+        sorted_population.append(uniq_population[cnt])
+        cnt += 1
 
-    for i in range(len(population)):
-        print("суммa расстояний", population[i], all_distance(population[i]))
-
+    print("поколение {0}:".format(_ + 2))
+    for i in range(len(sorted_population)):
+        print("суммa расстояний", sorted_population[i], all_distance(sorted_population[i]))
+    population = sorted_population
+print("\nКротчайший путь:", population[0], all_distance(population[0]))
